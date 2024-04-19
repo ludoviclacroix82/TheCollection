ed2008a1e0c50c3a5e6101484a4f0b9ec19796e4
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-04-19 15:03:20 +0200

    modifierd css - index - js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,7 @@ document.addEventListener('mouseover', function(event) {
     if (event.target.classList.contains('toogleLink')) {
         event.preventDefault();
 
-        var parentTarget = document.querySelector('nav').children;
+        var parentTarget = document.querySelector('.nav-subLink').children;
             for (const navChildren of parentTarget) {
                 const classChildren = navChildren.classList;
                 if(classChildren !== targetId){
@@ -17,7 +17,12 @@ document.addEventListener('mouseover', function(event) {
         document.querySelector("."+ targetId).classList.toggle('nav-open');  
     } 
            
-   
+    document.addEventListener('mouseout', function(event){
+        const subLink = document.querySelector('nav-subLink');
+        //const subLinkChildren = document.querySelector('.nav-subLink').children;
+        subLink.remove('nav-open');
+        console.log(subLinkChildren);
+    });
     
 });
 
