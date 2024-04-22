a8456f3e5ad069a325875f9131042c293364e38d
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-04-22 14:59:59 +0200

    modified navigation

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,4 @@
-document.addEventListener('mouseover', function(event) {
+document.addEventListener('mouseover', event => {
     if (event.target.classList.contains('toogleLink')) {
         event.preventDefault();
 
@@ -15,12 +15,24 @@ document.addEventListener('mouseover', function(event) {
         var targetId = event.target.getAttribute('data-target');
         console.log(targetId);        
         document.querySelector("."+ targetId).classList.toggle('nav-open');  
-    }              
+    }    
+    document.addEventListener('mouseout', event => { 
+        const navSubLink = document.querySelector('.nav-subLink');
+        if (event.target.nodeName === 'DIV') {  
+            const navLinks = navSubLink.querySelectorAll('div');
+            //console.log(navLinks);
+            for (const navLink of navLinks) {
+                navLink.classList.remove('nav-open');
+                //console.log(navLink);
+            }
+        }
+    });   
+
 });
 
 const navSubLink = document.querySelector('.nav-subLink');
 //console.log(navSubLink);
-navSubLink.addEventListener('mouseleave', event => {
+navSubLink.addEventListener('mouseout', event => {
     if (event.target.nodeName === 'DIV') {  
         const navLinks = navSubLink.querySelectorAll('div');
         //console.log(navLinks);
